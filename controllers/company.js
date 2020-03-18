f3b9aa5836be641d3e34b765c59740bd11730674
Rsingh2805 <rs280599@gmail.com>
2020-03-18 11:43:28 +0530

    Added add companies

--- controllers/company.js
+++ controllers/company.js
@@ -9,3 +9,21 @@ exports.getCompanies = function(req, res, next) {
         return res.status(200).json(companies)
     })
 }
+
+exports.addCompany = function(req, res, next){
+    const name = req.body.name
+    const image = req.body.image
+    if(!name)
+        return res.status(422).json({name: "Name is required"})
+    if(!image)
+        return res.status(422).json({name: "Image is required"})
+    let company = new Company({
+        name: name,
+        image: image
+    })
+    company.save(function(err, company){
+        if(err)
+            return next(err)
+        res.status(201).json(company)
+    })
+}
