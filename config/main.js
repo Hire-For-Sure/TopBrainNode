1118b6a1698a8e8d0f59e8aae020eebeb1806cb3
ankuryd <50947365+ankuryd@users.noreply.github.com>
2020-03-31 23:26:22 +0530

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,8 +1,12 @@
 module.exports = {
   // Secret key for JWT signing and encryption
-  'secret': 'super secret passphrase',
+  'secret': 'super secret passphrase for sqill',
   // Database connection information
-  'database': 'mongodb://localhost:27017/squil',
+  'database': 'mongodb://localhost:27017/sqill',
   // Setting port for server
   'port': 8888
+  // AWS S3 information
+  'AWS_ACCESS_KEY_ID': '',
+  'AWS_SECRET_ACCESS_KEY': '',
+  'AWS_REGION': ''
 }
