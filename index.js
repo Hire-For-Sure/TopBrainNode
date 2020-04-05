542f4bfc2685dc89a6596c441a3864c88d03a88f
ankuryd <50947365+ankuryd@users.noreply.github.com>
2020-04-05 22:50:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ console.log('Your server is running on port ' + config.port + '.')
 // Enable CORS for api calls
 app.use(cors())
 
+// Enable CORS from client-side
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials")
+  res.header("Access-Control-Allow-Credentials", "true")
+  next()
+})
+
 // Setting up basic middleware for all Express requests
 app.use(logger('dev')) // Log requests to API using morgan
 app.use(bodyParser.urlencoded({ extended: false }))
